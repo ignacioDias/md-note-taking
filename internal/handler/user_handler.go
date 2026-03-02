f2037b52d6d291a3f7584092aa8f3906be6aa155
ignacioDias <ignadiasg@gmail.com>
2026-03-02 18:27:28 -0300

    middleware implementation

--- internal/handler/user_handler.go
+++ internal/handler/user_handler.go
@@ -4,19 +4,11 @@ import (
 	"html/template"
 	"mdeditor/internal/database"
 	"net/http"
-
-	"golang.org/x/oauth2"
 )
 
 type UserHandler struct {
-	userRepo *database.UserRepository
-	config   *oauth2.Config
-}
-
-type userInfo struct {
-	ID    string `json:"id"`
-	Email string `json:"email"`
-	Name  string `json:"name"`
+	userRepo    *database.UserRepository
+	sessionRepo *database.SessionRepository
 }
 
 func (userHandler *UserHandler) LoginHandler(w http.ResponseWriter, r *http.Request) {
